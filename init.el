2980929c60df0417c9146708232b4c2f2f9f077d
granddaifuku <daifukuformac@gmail.com>
2021-05-06 19:14:37 +0900

    org-capture setting

--- init.el
+++ init.el
@@ -137,6 +137,36 @@
   :ensure t
   :after ox)
 
+;; org capture
+(with-eval-after-load 'org-capture
+  (defun org-hugo-new-subtree-post-capture-template ()
+    "Returns `org-capture' template string for new Hugo post.
+See `org-capture-templates' for more information."
+    (let* ((title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
+           (fname (org-hugo-slug title))
+		   (date (format-time-string "%Y-%m-%d" (org-current-time)))
+		   (section (format-time-string "%Y/%m" (org-current-time))))
+      (mapconcat #'identity
+                 `(
+                   ,(concat "* TODO " title)
+                   ":PROPERTIES:"
+                   ,(concat ":EXPORT_FILE_NAME: " fname)
+				   ,(concat ":EXPORT_DATE: " date)
+				   ,(concat ":EXPORT_HUGO_SECTION*: " section)
+                   ":END:"
+                   "%?\n")          ;Place the cursor here finally
+                 "\n")))
+
+  (add-to-list 'org-capture-templates
+               '("h"                ;`org-capture' binding + h
+                 "Hugo post"
+                 entry
+                 ;; It is assumed that below file is present in `org-directory'
+                 ;; and that it has a "Blog Ideas" heading. It can even be a
+                 ;; symlink pointing to the actual location of all-posts.org!
+                 (file+olp "blog.org" "Blog Ideas")
+                 (function org-hugo-new-subtree-post-capture-template))))
+
 
 ;;;;; Coding Style ;;;;;
 
