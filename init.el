324f3bbedd8133b58ba01f531f5e2038fe946ccf
granddaifuku <daifukuformac@gmail.com>
2022-05-28 23:50:30 +0900

    feat: remove unused functions

--- init.el
+++ init.el
@@ -406,26 +406,6 @@ See `org-capture-templates' for more information."
 
 
 ;;;;; flymake ;;;;;
-(use-package flymake
-  :init
-  (defun flymake-cc-init ()
-	(let* ((temp-file   (flymake-proc-init-create-temp-buffer-copy
-						 'flymake-create-temp-inplace))
-           (local-file  (file-relative-name
-						 temp-file
-						 (file-name-directory buffer-file-name))))
-      (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
-  :bind (
-		 ("C-c ! p" . flymake-goto-next-error)
-		 ("C-c ! n" . flymake-goto-prev-error))
-  :config
-  (push '("\\.cc$" flymake-cc-init) flymake-proc-allowed-file-name-masks)
-  (push '("\\.cpp$" flymake-cc-init) flymake-proc-allowed-file-name-masks)
-  (push '("\\.h$" flymake-cc-init) flymake-proc-allowed-file-name-masks)
-  (push '("\\.hpp$" flymake-cc-init) flymake-proc-allowed-file-name-masks)
-  (add-hook 'c++-mode-hook '(lambda () (flymake-mode t)))
-  )
-
 (use-package flymake-diagnostic-at-point
   :ensure t
   :after flymake
