f16c63c36d65c6fbb538be2772db095a8373c1cf
granddaifuku <daifukuformac@gmail.com>
2024-07-24 01:20:29 +0900

    fix

--- init.el
+++ init.el
@@ -5,8 +5,7 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")
-	("melpa-stable" . "http://stable.melpa.org/packages/")
-	("org" . "http://orgmode.org/elpa/")))
+	("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 (package-initialize)
 
