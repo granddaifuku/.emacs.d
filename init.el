9d5c6c7c13c741e43f2a2fd1c7214be5f53a8cb6
granddaifuku <daifukuformac@gmail.com>
2025-06-22 19:21:13 +0900

    Remove custom rainbow delimeters function

--- init.el
+++ init.el
@@ -539,20 +539,7 @@
 (use-package rainbow-delimiters
   :ensure t
   :config
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  (use-package cl-lib
-	:ensure t)
-  (use-package color
-	:ensure t
-	:config
-	(defun rainbow-delimiters-using-stronger-colors ()
-	  (interactive)
-	  (cl-loop
-	   for index from 1 to rainbow-delimiters-max-face-count
-	   do
-	   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-		 (cl-callf color-saturate-name (face-foreground face) 30)))))
-  (add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors))
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 ;; Cursor
 (use-package beacon
