bdbd35784f2221acbd27823eb255d985feb11086
granddaifuku <daifukuformac@gmail.com>
2025-11-02 23:08:21 +0900

    Remove unnecessary kind-icon setting

--- init.el
+++ init.el
@@ -672,8 +672,6 @@
 (use-package kind-icon
   :ensure t
   :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default)
   :config
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
